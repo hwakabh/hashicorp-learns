a7fc8e602326f9752d9029fc24ff36c401658d9a
hwakabh <hrykwkbys1024@gmail.com>
2024-11-07 11:59:06 +0900

    chore: removed TODO and made tidy.

--- setup-vault-gke/main.tf
+++ setup-vault-gke/main.tf
@@ -6,8 +6,6 @@ data "google_client_config" "provider" {}
 provider "google" {
   project = "hc-8732d2178369440c886cb59aee6"
   region  = "asia-east1"
-  // TODO: Need to replace with DPC
-  // Passed terraform service-account credentials via GOOGLE_CREDENTIALS
 }
 
 data "google_container_cluster" "my_cluster" {
@@ -20,7 +18,8 @@ provider "helm" {
         host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
         token = data.google_client_config.provider.access_token
         cluster_ca_certificate = base64decode(
-        data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,)
+          data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+        )
         exec {
             api_version = "client.authentication.k8s.io/v1beta1"
             command     = "gke-gcloud-auth-plugin"
@@ -28,9 +27,9 @@ provider "helm" {
     }
 }
 
+// https://github.com/hashicorp/vault-helm
 resource "helm_release" "vault_ent" {
   name              = "vault-ent"
-  // https://github.com/hashicorp/vault-helm
   repository        = "https://helm.releases.hashicorp.com"
   chart             = "vault"
   // Need to create namespace before tf-apply
