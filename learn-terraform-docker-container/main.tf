7ef0ac7f4f5e2739251e020e05c06c97ae018dd7
hwakabh <hrykwkbys1024@gmail.com>
2024-11-13 14:00:27 +0900

    chore: uploaded stuffs of learn-terraform-docker-container.

---
+++ learn-terraform-docker-container/main.tf
@@ -0,0 +1,38 @@
+// https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+// Pull docker image
+resource "docker_image" "img" {
+  name         = "nginx:1.27-alpine-slim"
+  keep_locally = true
+}
+
+// Run container
+resource "docker_container" "nginx-alice" {
+  image = docker_image.img.image_id
+  name  = "nginx-01"
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
+
+resource "docker_container" "nginx-bob" {
+  image = docker_image.img.image_id
+  name  = "nginx-02"
+
+  ports {
+    internal = 80
+    external = 8082
+  }
+}
