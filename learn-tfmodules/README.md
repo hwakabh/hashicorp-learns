a610de52d068d2f3f08feb32559dba15d8c987d7
hwakabh <hrykwkbys1024@gmail.com>
2024-11-30 21:59:53 +0900

    feat(WIP): added learnings of terraform modules

--- learn-tfmodules/README.md
+++ learn-tfmodules/README.md
@@ -1,5 +1,13 @@
 # learn-tfmodule
 
+## cloud-storage module (simple_bucket + multiple_buckets)
+<https://registry.terraform.io/modules/terraform-google-modules/cloud-storage/google/latest>
+
+## tarfile module
+Develop simple modules for file creation
+Using `hashicorp/local` provider + `hashicorp/archive` provider
+<https://registry.terraform.io/providers/hashicorp/local/latest>
+<https://registry.terraform.io/providers/hashicorp/archive/latest>
 
 ## tfc-factory-module
 Also learnings for TFC provider
--- learn-tfmodules/main.tf
+++ learn-tfmodules/main.tf
@@ -5,11 +5,6 @@ terraform {
       source  = "hashicorp/local"
       version = "2.5.2"
     }
-
-    google = {
-      source = "hashicorp/google"
-      version = "6.12.0"
-    }
   }
 }
 
@@ -27,16 +22,23 @@ resource "local_file" "world" {
   content  = "world, ${var.user_id} from ${var.user_company}"
 }
 
-// Invoking public modules
-module "gcs_buckets" {
+// Invoking public modules named with `my_buckets`
+module "my_buckets" {
   // https://registry.terraform.io/modules/terraform-google-modules/cloud-storage/google/latest
   source = "terraform-google-modules/cloud-storage/google"
   version = "8.0.1"
 
   project_id = "hc-8b1ddb1733494af2af02d477176"
-  // needs `prefix` for multiple creation
   names = ["bucket1", "mybucket2"]
+  // names needs `prefix` for multiple creation
   prefix = "hw"
+}
+
+// use outputs of modules
+resource "local_file" "result" {
+  // https://github.com/terraform-google-modules/terraform-google-cloud-storage/blob/master/outputs.tf#L61
+  content = module.my_buckets.urls_list[1]
+  filename = "module_executed_result.md"
 }
 
 // Call self-developed modules
