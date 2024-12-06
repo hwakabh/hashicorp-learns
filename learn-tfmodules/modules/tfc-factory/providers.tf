af92358132b7df31c2da9d6b191e43def72d00d9
hwakabh <hrykwkbys1024@gmail.com>
2024-12-07 01:50:39 +0900

    chore: added provider deps to tfe-factory module.

---
+++ learn-tfmodules/modules/tfc-factory/providers.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    // https://registry.terraform.io/providers/hashicorp/tfe/latest/docs
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.60.1"
+    }
+  }
+}
