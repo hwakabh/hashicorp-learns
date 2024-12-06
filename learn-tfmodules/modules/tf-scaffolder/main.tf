cf6ff298e51a381b4f0d2a9c834ed9fe673ce91b
hwakabh <hrykwkbys1024@gmail.com>
2024-12-07 00:41:45 +0900

    fix: updated target path to operate.

--- learn-tfmodules/modules/tf-scaffolder/main.tf
+++ learn-tfmodules/modules/tf-scaffolder/main.tf
@@ -1,11 +1,11 @@
 // using 'hashicorp/local' provider
 resource "local_file" "readme" {
-  filename = "${path.module}/${var.project_name}/README.md"
+  filename = "${path.root}/${var.project_name}/README.md"
   content  = "# ${var.project_name}"
 }
 
 resource "local_file" "providers" {
-  filename = "${path.module}/${var.project_name}/providers.tf"
+  filename = "${path.root}/${var.project_name}/providers.tf"
   content  = <<-EOF
     terraform {
       required_providers {
@@ -19,12 +19,12 @@ resource "local_file" "providers" {
 }
 
 resource "local_file" "main" {
-  filename = "${path.module}/${var.project_name}/main.tf"
+  filename = "${path.root}/${var.project_name}/main.tf"
   content  = ""
 }
 
 resource "local_file" "variables" {
-  filename = "${path.module}/${var.project_name}/variables.tf"
+  filename = "${path.root}/${var.project_name}/variables.tf"
   content  = ""
 }
 
@@ -39,7 +39,7 @@ resource "archive_file" "this" {
     local_file.variables,
   ]
   type        = "tar.gz"
-  source_dir  = "${path.module}/${var.project_name}"
-  output_path = "${path.module}/${var.project_name}.tar.gz"
+  source_dir  = "${path.root}/${var.project_name}"
+  output_path = "${path.root}/${var.project_name}.tar.gz"
   // note that this archive_file resource and directory created do not assure idempotency in destroy
 }
